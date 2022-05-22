da5be950f7cce439885ed1b54362b5864fc3c610
Mina Karam <minakaram.me@gmail.com>
2022-05-22 04:15:52 +0200

    Update Dio.c

--- bsw/static/Dio.c
+++ bsw/static/Dio.c
@@ -1,8 +1,27 @@
 /*
- * Dio.c
+ * Module: Dio
  *
- *  Created on: May 21, 2022
- *      Author: Mina-Karam
+ * File Name: Dio.c
+ *
+ * Description: Source file for Dio Module on STM32F103x6,
+ * 				Implementation is based on AUTSAR Specifications.
+ * Created on: May 21, 2022
+ *
+ * Author: Mina-Karam
  */
 
-/* Dio.c shall include Det.h if detection of development error (DET) is enabled. */
+/* ================================================================ */
+/* =========================== Includes =========================== */
+/* ================================================================ */
+#include "Dio.h"
+
+#if (DIO_DEV_ERROR_DETECT == STD_ON)
+	#include "Det.h"
+
+/* AUTOSAR Version checking between Det and Dio Modules */
+#if ((DET_AR_MAJOR_VERSION != DIO_AR_RELEASE_MAJOR_VERSION)\
+ || (DET_AR_MINOR_VERSION != DIO_AR_RELEASE_MINOR_VERSION)\
+ || (DET_AR_PATCH_VERSION != DIO_AR_RELEASE_PATCH_VERSION))
+  #error "The AR version of Det.h does not match the expected version"
+#endif
+
