ce1b1845d9c7fbef019c7942f6b19d53b09136dc
Mina Karam <minakaram.me@gmail.com>
2022-05-22 04:17:14 +0200

    Add Det.c

---
+++ Det.c
@@ -0,0 +1,28 @@
+/*
+ * Module: Default Error Tracer
+ *
+ * File Name: Det.c
+ *
+ * Description: Det stores the development errors reported by other modules.
+ *
+ * Created on: May 22, 2022
+ *
+ * Author: Mina-Karam
+ */
+
+/* ================================================================ */
+/* =========================== Includes =========================== */
+/* ================================================================ */
+
+#include "Det.h"
+
+void Det_Init()
+{
+	//TODO
+}
+
+Std_ReturnType Det_ReportError(uint16 ModuleId, uint8 InstanceId, uint8 ApiId, uint8 ErrorId )
+{
+	//TODO
+	return E_OK;
+}
